a4ed4a1e8abdcf65fc67dacd8b89a89960ef7adc
Shubham Patel <120495166+shubham26062002@users.noreply.github.com>
2023-06-25 15:09:03 +0530

    Frontend work started, Navbar completed.

---
+++ theme-bucket-frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": ["Poppins", "sans-serif"],
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
+      },
+      screens: {
+        "mobile": "360px",
+        "desktop": "1180px",
+      },
+      colors: {
+        "black-2": "#232323",
+        "brown": "#504538",
+        "yellow-ochre": "#E8AA3D"
+      }
+    },
+  },
+  plugins: [],
+}
+
