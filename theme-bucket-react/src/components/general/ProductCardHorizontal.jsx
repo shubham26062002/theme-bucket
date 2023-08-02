30203155dba5f59fe7210b69ea9adb5aca2b1ca7
Shubham Patel <120495166+shubham26062002@users.noreply.github.com>
2023-08-02 14:57:37 +0530

    Added download feature.

--- theme-bucket-react/src/components/general/ProductCardHorizontal.jsx
+++ theme-bucket-react/src/components/general/ProductCardHorizontal.jsx
@@ -12,53 +12,77 @@ const ProductCardHorizontal = ({
     avgRating,
     ratingsCount,
     price,
+    createdAt,
+    srcUrl
 }) => {
+    const download = async () => {
+        window.location.assign(srcUrl)
+    }
+
+    const addToCart = async () => {
+
+    }
+
+    const editProduct = async () => {
+
+    }
+
     return (
-        <Link className="group" to={to}>
-            <div
-                className="grid mobile:grid-cols-1 desktop:grid-cols-2 shadow-product-shadow shadow-gray-300 rounded-lg overflow-hidden group-hover:shadow-md group-hover:-translate-y-1 transition">
+        <div
+            className="grid mobile:grid-cols-1 desktop:grid-cols-2 shadow-product-shadow shadow-gray-300 rounded-lg overflow-hidden group-hover:shadow-md group-hover:-translate-y-1 transition">
+            <Link className="group" to={to}>
                 <img className="block aspect-video" src={imageUrl}
                     alt={name} />
-                <div className="p-4 self-center">
-                    <h1
-                        className="font-semibold text-neutral-700 whitespace-nowrap overflow-hidden text-ellipsis max-w-full leading-relaxed desktop:text-lg">
-                        {name}</h1>
+            </Link>
+            <div className="p-4 self-center">
+                <h1
+                    className="font-semibold text-neutral-700 whitespace-nowrap overflow-hidden text-ellipsis max-w-full leading-relaxed desktop:text-lg">
+                    {name}</h1>
 
-                    {((type === 'product')||(type === 'purchased') &&
-                        <p
-                            className="text-xs text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis max-w-full desktop:text-sm">
-                            by {publisherName}</p>
-                    )}
+                {((type === 'product') || (type === 'purchased') &&
+                    <p
+                        className="text-xs text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis max-w-full desktop:text-sm">
+                        by {publisherName}</p>
+                )}
 
-                    <div className="mt-3 flex justify-between items-center">
-                        <div className="flex justify-start items-center gap-1.5">
-                            <span
-                                className="text-sm desktop:text-base font-semibold text-neutral-700 leading-none">{avgRating}</span>
-                            <BsStarFill className="flex justify-center items-center w-4 h-4 desktop:w-5 desktop:h-5 text-yellow-ochre" />
-                            <span
-                                className="text-xs desktop:text-sm font-light text-neutral-500 leading-none">({ratingsCount})</span>
-                        </div>
-                        <p className="font-bold text-lg desktop:text-xl text-neutral-700">Rs. {price}
-                        </p>
-                    </div>
-                    <button
-                        className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition">
-
-                        {type === 'product' ? (
-                            <FiShoppingBag className="flex justify-center items-center h-5 w-5 text-brown" />
-                        ) :type === 'purchased' ?
-                        (
-                            <AiOutlineDownload className="flex justify-center items-center h-5 w-5 text-brown" />
-                        ):(
-                            <FiEdit className="flex justify-center items-center h-5 w-5 text-brown" />
-                        )}
-
+                <div className="mt-3 flex justify-between items-center">
+                    <div className="flex justify-start items-center gap-1.5">
                         <span
-                            className="font-bold uppercase text-xs tracking-widest text-neutral-700">{type === 'product' ? 'Add To Cart' : type==='purchased'?'Download':'Edit'}</span>
-                    </button>
+                            className="text-sm desktop:text-base font-semibold text-neutral-700 leading-none">{avgRating}</span>
+                        <BsStarFill className="flex justify-center items-center w-4 h-4 desktop:w-5 desktop:h-5 text-yellow-ochre" />
+                        <span
+                            className="text-xs desktop:text-sm font-light text-neutral-500 leading-none">({ratingsCount})</span>
+                    </div>
+                    <p className="font-bold text-lg desktop:text-xl text-neutral-700">Rs. {price}
+                    </p>
                 </div>
+
+                {type === 'product' ? (
+                    <button type="button" className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition" onClick={addToCart}>
+                        <FiShoppingBag className="flex justify-center items-center h-5 w-5 text-brown" />
+                        <span
+                            className="font-bold uppercase text-xs tracking-widest text-neutral-700">Add To Cart</span>
+                    </button>
+                ) : type === 'purchased' ? (
+                    <button type="button" className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition" onClick={download}>
+                        <AiOutlineDownload className="flex justify-center items-center h-5 w-5 text-brown" />
+                        <span
+                            className="font-bold uppercase text-xs tracking-widest text-neutral-700">Download</span>
+                    </button>
+                ) : (
+                    <button type="button" className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition" onClick={editProduct}>
+                        <FiEdit className="flex justify-center items-center h-5 w-5 text-brown" />
+                        <span
+                            className="font-bold uppercase text-xs tracking-widest text-neutral-700">Edit</span>
+                    </button>
+                )}
+
+                {type === 'purchased' && (
+                    <p className="mt-2 text-xs text-gray-500">Purchased on {new Date(createdAt).getDate()}-{new Date(createdAt).getMonth() + 1}-{new Date(createdAt).getFullYear()},</p>
+                )}
             </div>
-        </Link>
+        </div >
+
     )
 }
 
--- theme-bucket-react/src/pages/Product.jsx
+++ theme-bucket-react/src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router'
 import ProductDetails from '../components/product/ProductDetails'
 import ProductImages from '../components/product/ProductImages'
-import {supabase } from '../libs/supabase-client'
+import { supabase } from '../libs/supabase-client'
 import { useEffect, useState } from 'react'
 
 const Product = () => {
@@ -13,7 +13,7 @@ const Product = () => {
             const { data, error } = await supabase
                 .from('products')
                 .select(`*,profiles(full_name),ratings(count),categories(name)`)
-               .eq("id", productId)
+                .eq("id", productId)
             setproduct(data[0])
         }
         getproduct()
@@ -21,92 +21,92 @@ const Product = () => {
     console.log(product)
     return (
         <>
-    
-         <main class="bg-gray-lightest p-2">
-        <div class="bg-white shadow-sm shadow-gray-400 rounded-md h-full py-12 px-6 desktop:px-28">
-            <div class="grid grid-cols-1 desktop:grid-cols-3 desktop:gap-8">
-                <div className="flex flex-col desktop:flex-row gap-6 desktop:col-start-1 desktop:col-end-3">
-                        <ProductImages topimage="/images/blog-website.jpg"/>
-                    <div class="mt-14 mb-3 desktop:mt-3">
-                        <button>
-                    
-                            <svg className="flex justify-center items-center stroke-2 stroke-rose-500 fill-rose-500 h-7 w-7"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-heart">
-                                <path
-                                    d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
-                                </path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <h1 class="text-2xl font-bold text-neutral-700 leading-normal">
-                    {product.name}</h1>
-                    <p class="text-gray-500">
-                    by {product.profiles?.full_name}</p>
-                    <div class="mt-4 flex justify-start items-center gap-1.5">
-                        <span class="font-semibold text-neutral-700 leading-none">{product.avg_rating || 0}</span>
-                        <svg className="flex justify-center items-center w-5 h-5 fill-yellow-ochre stroke-yellow-ochre"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-star">
-                            <polygon
-                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
-                            </polygon>
-                        </svg>
-                        <span class="font-light text-neutral-500 leading-none">({product.ratings?product.ratings[0].count:0 })</span>
-                        {/* <span class="font-light text-neutral-500 leading-none">(100)</span> */}
-                    </div>
-                    <p class="mt-6 font-bold text-2xl text-neutral-700">Rs. {product.price}
-                    </p>
-                    <div class="space-y-3 pt-6 max-w-sm">
-                        <button
-                            class="inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition">
-                            <svg className="flex justify-start items-center h-5 w-5 stroke-brown"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-shopping-bag">
-                                <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
-                                <line x1="3" y1="6" x2="21" y2="6"></line>
-                                <path d="M16 10a4 4 0 0 1-8 0"></path>
-                            </svg>
-                            <span class="font-bold uppercase tracking-widest text-xs text-neutral-700">Add to
-                                Cart</span>
-                        </button>
-                        <a href="#"
-                            class="inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition">
-                            <svg className="flex justify-start items-center h-5 w-5 stroke-brown"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-layout">
-                                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                                <line x1="3" y1="9" x2="21" y2="9"></line>
-                                <line x1="9" y1="21" x2="9" y2="9"></line>
-                            </svg>
-                            <span class="font-bold uppercase tracking-widest text-xs text-neutral-700">Live
-                                Preview</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-14 grid grid-cols-1 desktop:grid-cols-3 gap-14">
-                <div class="desktop:col-start-1 desktop:col-end-3">
-                    <p class="text-lg font-bold text-black-2">
-                        Description</p>
-                    <div class="h-[2px] bg-gray-500 mt-2"></div>
-                    <div class="py-6 space-y-5">
-                        <p class="text-neutral-600 text-sm leading-relaxed">{product.description}</p>
-                    </div>
-                </div>
-                <ProductDetails toolstack={product.tools_stack} compatiblebrowsers={product.compatible_browsers} category={product.categories?.name} publishedat={product.created_at} responsive={product.is_responsive}/>
-                </div>
-         
-                </div>
-                </main>
 
-       
+            <main class="bg-gray-lightest p-2">
+                <div class="bg-white shadow-sm shadow-gray-400 rounded-md h-full py-12 px-6 desktop:px-28">
+                    <div class="grid grid-cols-1 desktop:grid-cols-3 desktop:gap-8">
+                        <div className="flex flex-col desktop:flex-row gap-6 desktop:col-start-1 desktop:col-end-3">
+                            <ProductImages topimage="/images/blog-website.jpg" />
+                            <div class="mt-14 mb-3 desktop:mt-3">
+                                <button>
+
+                                    <svg className="flex justify-center items-center stroke-2 stroke-rose-500 fill-rose-500 h-7 w-7"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-heart">
+                                        <path
+                                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 class="text-2xl font-bold text-neutral-700 leading-normal">
+                                {product.name}</h1>
+                            <p class="text-gray-500">
+                                by {product.profiles?.full_name}</p>
+                            <div class="mt-4 flex justify-start items-center gap-1.5">
+                                <span class="font-semibold text-neutral-700 leading-none">{product.avg_rating || 0}</span>
+                                <svg className="flex justify-center items-center w-5 h-5 fill-yellow-ochre stroke-yellow-ochre"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                    class="feather feather-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                    </polygon>
+                                </svg>
+                                <span class="font-light text-neutral-500 leading-none">({product.ratings ? product.ratings[0].count : 0})</span>
+                                {/* <span class="font-light text-neutral-500 leading-none">(100)</span> */}
+                            </div>
+                            <p class="mt-6 font-bold text-2xl text-neutral-700">Rs. {product.price}
+                            </p>
+                            <div class="space-y-3 pt-6 max-w-sm">
+                                <button
+                                    class="inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition">
+                                    <svg className="flex justify-start items-center h-5 w-5 stroke-brown"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-shopping-bag">
+                                        <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
+                                        <line x1="3" y1="6" x2="21" y2="6"></line>
+                                        <path d="M16 10a4 4 0 0 1-8 0"></path>
+                                    </svg>
+                                    <span class="font-bold uppercase tracking-widest text-xs text-neutral-700">Add to
+                                        Cart</span>
+                                </button>
+                                <a href="#"
+                                    class="inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition">
+                                    <svg className="flex justify-start items-center h-5 w-5 stroke-brown"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-layout">
+                                        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                                        <line x1="3" y1="9" x2="21" y2="9"></line>
+                                        <line x1="9" y1="21" x2="9" y2="9"></line>
+                                    </svg>
+                                    <span class="font-bold uppercase tracking-widest text-xs text-neutral-700">Live
+                                        Preview</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-14 grid grid-cols-1 desktop:grid-cols-3 gap-14">
+                        <div class="desktop:col-start-1 desktop:col-end-3">
+                            <p class="text-lg font-bold text-black-2">
+                                Description</p>
+                            <div class="h-[2px] bg-gray-500 mt-2"></div>
+                            <div class="py-6 space-y-5">
+                                <p class="text-neutral-600 text-sm leading-relaxed">{product.description}</p>
+                            </div>
+                        </div>
+                        <ProductDetails toolstack={product.tools_stack} compatiblebrowsers={product.compatible_browsers} category={product.categories?.name} publishedat={product.created_at} responsive={product.is_responsive} />
+                    </div>
+
+                </div>
+            </main>
+
+
 
 
         </>
