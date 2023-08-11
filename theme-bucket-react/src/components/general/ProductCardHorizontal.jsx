c41222ebc3120c508987f2d820a1e6cdd9388f84
Meet Thummar <meetthummar132@gmail.com>
2023-08-11 22:40:46 +0530

    category products complete

--- theme-bucket-react/src/components/general/ProductCardHorizontal.jsx
+++ theme-bucket-react/src/components/general/ProductCardHorizontal.jsx
@@ -7,7 +7,7 @@ const ProductCardHorizontal = ({
     to,
     imageUrl,
     name,
-    type = 'product',
+    cardType = 'product',
     publisherName,
     avgRating,
     ratingsCount,
@@ -36,16 +36,16 @@ const ProductCardHorizontal = ({
                     className="font-semibold text-neutral-700 whitespace-nowrap overflow-hidden text-ellipsis max-w-full leading-relaxed desktop:text-lg">
                     {name}</h1>
 
-                {((type === 'product') || (type === 'purchased') &&
+                {(((cardType === 'product') || (cardType === 'purchased') )&&(
                     <p
                         className="text-xs text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis max-w-full desktop:text-sm">
                         by {publisherName}</p>
-                )}
+                ))}
 
                 <div className="mt-3 flex justify-between items-center">
                     <div className="flex justify-start items-center gap-1.5">
                         <span
-                            className="text-sm desktop:text-base font-semibold text-neutral-700 leading-none">{avgRating}</span>
+                            className="text-sm desktop:text-base font-semibold text-neutral-700 leading-none">{avgRating? avgRating : 0}</span>
                         <BsStarFill className="flex justify-center items-center w-4 h-4 desktop:w-5 desktop:h-5 text-yellow-ochre" />
                         <span
                             className="text-xs desktop:text-sm font-light text-neutral-500 leading-none">({ratingsCount})</span>
@@ -54,13 +54,13 @@ const ProductCardHorizontal = ({
                     </p>
                 </div>
 
-                {type === 'product' ? (
+                {cardType === 'product' ? (
                     <button type="button" className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition" onClick={addToCart}>
                         <FiShoppingBag className="flex justify-center items-center h-5 w-5 text-brown" />
                         <span
                             className="font-bold uppercase text-xs tracking-widest text-neutral-700">Add To Cart</span>
                     </button>
-                ) : type === 'purchased' ? (
+                ) : cardType === 'purchased' ? (
                     <button type="button" className="mt-8 inline-flex w-full justify-center items-center gap-2 py-2 rounded-md border-[1px] border-gray-300 hover:bg-gray-50 hover:border-gray-500 transition" onClick={download}>
                         <AiOutlineDownload className="flex justify-center items-center h-5 w-5 text-brown" />
                         <span
@@ -74,7 +74,7 @@ const ProductCardHorizontal = ({
                     </Link>
                 )}
 
-                {type === 'purchased' && (
+                {cardType === 'purchased' && (
                     <p className="mt-2 text-xs text-gray-500">Purchased on {new Date(createdAt).getDate()}-{new Date(createdAt).getMonth() + 1}-{new Date(createdAt).getFullYear()},</p>
                 )}
             </div>
